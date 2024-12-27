cdce5a03cdc0eb84d7e4093a016b746d2e598802
bsodocean <socajowa3@gmail.com>
2024-12-27 09:21:16 +0100

    Moved API logic to routes, updated Prisma schema

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,51 +5,122 @@ import prisma from "../prisma/prismaClientSingleton";
 
 const router = Router();
 
-router.get("/api/getGoodies", (req: Request, res: Response) => {
-  asyncHandler(async (req, res) => {
+router.get(
+  "/api/getGoodies",
+  asyncHandler(async (req: Request, res: Response) => {
     const goodies = await prisma.goodie.findMany();
     res.status(200).json(goodies);
-  });
+  })
+);
 
-  router.post("/api/createGoodie", (req: Request, res: Response) => {
-    asyncHandler(async (req, res) => {
-      const { id, name, price, createdAt } = req.body;
+router.post(
+  "/api/createGoodie",
+  asyncHandler(async (req: Request, res: Response) => {
+    const { id, name, price, amount, createdAt } = req.body;
 
-      const newGoodie: Goodie = await prisma.goodie.create({
+    const newGoodie: Goodie = await prisma.goodie.create({
+      data: {
+        id,
+        name,
+        price,
+        amount,
+        createdAt,
+      },
+    });
+    res.status(201).json(newGoodie);
+  })
+);
+
+router.patch(
+  "/api/updateGoodie/:id",
+  asyncHandler(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const updates = req.body;
+
+    if (!updates || Object.keys(updates).length === 0) {
+      return res.status(400).json({ error: "No updates provided" });
+    }
+
+    const oldGoodie = await prisma.goodie.findUnique({ where: { id: id } });
+
+    const updatedGoodie = await prisma.goodie.update({
+      where: { id },
+      data: updates,
+    });
+
+    if (!oldGoodie) {
+      res.status(400).json({ Error: "Nope" });
+    } else {
+      await prisma.auditLog.create({
         data: {
-          id,
-          name,
-          price,
-          createdAt,
+          goodieId: id,
+          oldName: oldGoodie.name,
+          oldPrice: oldGoodie.price,
+          oldAmount: oldGoodie.amount,
+          timestamp: new Date(),
         },
       });
-      res.status(201).json(newGoodie);
+    }
+    res.status(200).json(updatedGoodie);
+  })
+);
+
+router.delete(
+  "/api/deleteGoodie/:id",
+  asyncHandler(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const goodieToDelete = await prisma.goodie.delete({ where: { id: id } });
+    !goodieToDelete
+      ? res.status(400).json({ Error: "No matching goodie found" })
+      : await prisma.goodie.delete({
+          where: { id },
+        });
+
+    res.status(200).send();
+  })
+);
+
+router.get(
+  "/api/getPriceHistory/:id",
+  asyncHandler(async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    const goodie = await prisma.auditLog.findMany({
+      where: { goodieId: id },
     });
-  });
 
-  router.patch(
-    "/api/updateGoodie/:id",
-    asyncHandler(async (req: Request, res: Response) => {
-      const { id } = req.params;
-      const updates = req.body;
+    let priceHistory: { editId: number; oldPrice: number; timestamp: Date }[] =
+      [];
 
-      if (!updates || Object.keys(updates).length === 0) {
-        return res.status(400).json({ error: "No updates provided" });
-      }
+    !goodie
+      ? res.status(400).json({ Error: "No goodie found" })
+      : (priceHistory = await prisma.auditLog.findMany({
+          where: { goodieId: id },
+          select: { editId: true, oldPrice: true, timestamp: true },
+        }));
+    console.log(priceHistory);
+    res.status(200).json(priceHistory);
+  })
+);
 
-      const updatedGoodie = await prisma.goodie.update({
-        where: { id },
-        data: updates,
-      });
+router.get(
+  "/api/lookupGoodie/:id",
+  asyncHandler(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const goodie = await prisma.goodie.findUnique({ where: { id: id } });
+    !goodie
+      ? res.status(400).json({ Error: "No goodie found:(" })
+      : res.status(200).json(goodie);
+  })
+);
 
-      res.status(200).json(updatedGoodie);
-    })
-  );
+router.get(
+  "/api/filterByQuantity",
+  asyncHandler(async (req: Request, res: Response) => {
+    const goodies = await prisma.goodie.findMany();
+    goodies.sort((a, b) => b.amount - a.amount);
+    res.status(200).json(goodies);
+  })
+);
 
-  router.get(
-    "/api/getPriceHistory/:id",
-    asyncHandler(async (req: Request, res: Response) => {
-      const { id } = req.params;
-    })
-  );
-});
+export default router;
